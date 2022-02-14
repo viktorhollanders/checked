8b6b1f729ee96f5a8cf4968ffef494ad63366387
Viktor Hollanders <viktorhollanders@hey.com>
2022-02-14 18:45:49 +0000

    add create new todo feature and todolist featur

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const containerApp = document.querySelector('.app');
+const containerActiveTodods = document.querySelector('.activeTodo');
+const containerCompletedTodo = document.querySelector('.completedTodo');
+
+const btnLogged = document.querySelector('.btn--logged');
+
+const inputAddTodo = document.querySelector('.addTodo__input--todo');
+const inputCheckBox = document.querySelector('.addTodo__input--cheskbox');
+
+inputAddTodo.addEventListener('keydown', function (e) {
+  if (e.code === 'Enter') {
+    e.preventDefault();
+
+    const newTodoHTML = `
+         <div class="todo__row">
+          <input class="form--check" type="checkbox" />
+          <p class="todo__content">${inputAddTodo.value}</p>
+         </div>`;
+
+    containerActiveTodods.insertAdjacentHTML('afterbegin', newTodoHTML);
+
+    inputAddTodo.value = '';
+    inputAddTodo.blur();
+  }
+});
+
+inputCheckBox.addEventListener('click', function (e) {
+  if (inputCheckBox.checked) {
+  }
+});

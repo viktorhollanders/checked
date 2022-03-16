134462610105aa3b8dad2a6458dc5c82f63279f7
Viktor Hollanders <viktorhollanders@hey.com>
2022-03-16 22:06:19 +0000

    refactor js code

--- script.js
+++ script.js
@@ -5,28 +5,44 @@ let todoList = [];
 const containerActive = document.querySelector('.activeTodos');
 const containerCompleted = document.querySelector('.completedTodos');
 
+// BUTTONS
+const btnShowCompletedTodos = document.querySelector('.btn--logged');
+
 // USER ACTIONS
 const inputUserAddTodo = document.querySelector('.addTodo__input--content');
+const inputUserDate = document.querySelector('.addTodo__date');
+
+function renderTodo(todo) {
+  const todoRowHTML = `
+      <form id="${todo.id}" class="form todo__row">
+        <input id="${todo.id}" class="form--check addTodo__input--checkbox" type="checkbox"/>
+        <p class="todo__content">${todo.text}</p>
+      </form>`;
+
+  containerActive.insertAdjacentHTML('afterbegin', todoRowHTML);
+}
 
 function createTodo(text) {
+  // create todo item
   const todo = {
     text,
-    checked: false,
     id: Date.now(),
   };
   todoList.push(todo);
+
+  // add todo to ui
+  renderTodo(todo);
 }
 
-function updateTodoList(list) {
-  const item = list.slice(-1);
-
-  const todoRowHTML = `
-    <form class="form todo__row" >
-    <input id="${item[0].id}" class="form--check addTodo__input--checkbox" type="checkbox" />
-    <p class="todo__content">${item[0].text}</p>
-    </form>`;
-
-  containerActive.insertAdjacentHTML('afterbegin', todoRowHTML);
+function updateLoggedCount() {
+  const completedTodosCount = containerCompleted.childElementCount;
+  if (completedTodosCount > 0) {
+    btnShowCompletedTodos.textContent = `Show ${completedTodosCount} logged item`;
+    btnShowCompletedTodos.classList.remove('hidden');
+  } else {
+    btnShowCompletedTodos.textContent = `Show ${completedTodosCount} logged item`;
+    btnShowCompletedTodos.classList.add('hidden');
+  }
 }
 
 inputUserAddTodo.addEventListener('keydown', function (e) {
@@ -36,6 +52,28 @@ inputUserAddTodo.addEventListener('keydown', function (e) {
 
     inputUserAddTodo.value = '';
     inputUserAddTodo.blur();
-    updateTodoList(todoList);
   }
 });
+
+// When a user checks an element log the element and status to the console
+
+document.addEventListener('click', function (e) {
+  if (
+    e.target.className == 'form--check addTodo__input--checkbox' &&
+    e.target.checked
+  ) {
+    const completedTodo = document.getElementById(`${e.target.id}`);
+    containerCompleted.appendChild(completedTodo);
+    completedTodo.classList.add('completed');
+    updateLoggedCount();
+  } else if (
+    e.target.className == 'form--check addTodo__input--checkbox' &&
+    !e.target.checked
+  ) {
+    const completedTodo = document.getElementById(`${e.target.id}`);
+    containerActive.appendChild(completedTodo);
+    completedTodo.classList.remove('completed');
+    updateLoggedCount();
+  }
+});
+

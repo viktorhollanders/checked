f0f2872bdd338ba45d44849368cb1118f10980ad
Viktor Hollanders <viktorhollanders@hey.com>
2022-05-29 22:46:09 +0000

    add enter site feature and animation

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 'use strict';
 
-const onboarding = document.querySelector('.onboarding');
 const title = document.querySelector('.title');
+const onboardingContainer = document.querySelector('.onboarding');
+const appContainer = document.querySelector('.app');
 const addTodoBtn = document.querySelector('.add-todo');
 
 window.onload = () => {
   userHasViseted();
 };
 
+function enterSite() {
+  onboardingContainer.style.display = 'none';
+  title.style.transform = 'translatey(0)';
+  title.style.margin = '50px 0 80px 0';
+  addTodoBtn.style.top = '46rem';
+  appContainer.style.display = 'block';
+  appContainer.style.opacity = '1';
+}
+
 function userHasViseted() {
   const hasVisited = JSON.parse(localStorage.getItem('viseted'));
   if (hasVisited === true) {
-    onboarding.style.display = 'none';
-    title.style.margin = '3.75rem 0rem 0rem 5rem';
-    addTodoBtn.style.top = '46rem';
-
-    document.querySelector('.app').style.display = 'block';
+    enterSite();
   } else {
     localStorage.setItem('viseted', JSON.stringify(true));
   }
 }
+
+addTodoBtn.addEventListener('click', function () {
+  enterSite();
+});
